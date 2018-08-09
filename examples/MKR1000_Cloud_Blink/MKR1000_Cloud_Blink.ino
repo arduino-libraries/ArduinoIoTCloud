3ddf07a1265866a0fc8dd155102e4940f0725f63
Alberto Iannaccone <a.iannaccone@arduino.cc>
2018-08-09 16:16:49 +0200

    blink via cloud monitor

--- examples/MKR1000_Cloud_Blink/MKR1000_Cloud_Blink.ino
+++ examples/MKR1000_Cloud_Blink/MKR1000_Cloud_Blink.ino
@@ -1,11 +1,11 @@
 #include <WiFi101.h>
 #include <ArduinoCloudV2.h>
 
-#include "arduino_secrets.h"
 ///////please enter your sensitive data in the Secret tab/arduino_secrets.h
 char ssid[] = SECRET_SSID;        // your network SSID (name)
 char pass[] = SECRET_PASS;    // your network password (use for WPA, or use as key for WEP)
 int status = WL_IDLE_STATUS;     // the WiFi radio's status
+String serialString = "";        // the string used to compose network messages from the received characters
 
 WiFiClient wifiClient;
 
@@ -13,41 +13,10 @@ unsigned long getTime() {
   return WiFi.getTime();
 }
 
-// Thing properties
-int position;
-bool valid = true;
-float ratio = 2.47;
-String welcome = "ciao";
-
-// Last time when the WiFi connection was checked
-unsigned long lastMillis = 0;
-
-void onPositionUpdate() {
-  Serial.print("New position value: ");
-  Serial.println(position);
-}
-
-void onWelcomeUpdate() {
-  Serial.print("New state value: ");
-  Serial.println(welcome);
-}
-
-void onValidUpdate() {
-  Serial.print("New valid value: ");
-  Serial.println(valid);
-}
-
-void onRatioUpdate() {
-  Serial.print("New ratio value: ");
-  Serial.println(ratio);
-}
-
 void setup() {
   //Initialize serial and wait for port to open:
   Serial.begin(9600);
-  while (!Serial) {
-    ; // wait for serial port to connect. Needed for native USB port only
-  }
+  delay(7000);
 
   // check for the presence of the shield:
   if (WiFi.status() == WL_NO_SHIELD) {
@@ -62,14 +31,20 @@ void setup() {
   }
 
   // attempt to connect to WiFi network:
-  while (status != WL_CONNECTED) {
+  int attempts = 0;
+  while (status != WL_CONNECTED && attempts < 6) {
     Serial.print("Attempting to connect to WPA SSID: ");
     Serial.println(ssid);
     // Connect to WPA/WPA2 network:
     status = WiFi.begin(ssid, pass);
 
     // wait 10 seconds for connection:
-    delay(5000);
+    delay(10000);
+    attempts++;
+  }
+
+  if (status != WL_CONNECTED) {
+    Serial.println("Failed to connect to Wifi!");
   }
 
   // you're connected now, so print out the data:
@@ -79,51 +54,82 @@ void setup() {
   Serial.println("Attempting to connect to Arduino Cloud ...");
 
   ArduinoCloud.onGetTime(getTime);
-  if (!ArduinoCloud.connect()) {
+
+  attempts = 0;
+  while (!ArduinoCloud.connect() && attempts < 10) {
+    attempts++;
+  }
+
+  if (attempts >= 3) {
     Serial.println("Failed to connect to Arduino Cloud!");
     while (1);
   }
 
   Serial.println("Successfully connected to Arduino Cloud :)");
-  ArduinoCloud.addProperty(welcome, READWRITE, ON_CHANGE, onWelcomeUpdate);
-  ArduinoCloud.addProperty(position, READWRITE, ON_CHANGE, onPositionUpdate);
-  ArduinoCloud.addProperty(valid, READWRITE, ON_CHANGE, onValidUpdate);
-  ArduinoCloud.addProperty(ratio, READWRITE, ON_CHANGE, onRatioUpdate);
 
   CloudSerial.begin(9600);
-  lastMillis = millis();
+  CloudSerial.print("I'm ready for blinking!\n");
 }
 
 void loop() {
   ArduinoCloud.poll();
-  Serial.println("loop updated");
- /* 
-  Serial.println(".");
-  welcome += "!";
-  ratio += 0.4355;
-  valid = !valid;
-  position += 1;
-*/
-  if (millis() - lastMillis > 20000) {
-    Serial.println("..Check WiFi status..");
-    bool error = false;
-    // Check Wifi status
-    while (WiFi.status() != WL_CONNECTED) {
-      error = true;
-      Serial.print("..Reconnection to connect to WPA SSID: ");
-      Serial.println(ssid);
-      status = WiFi.begin(ssid, pass);
-      // wait 10 seconds for connection:
-      delay(2000);
+
+  // check if there is something waiting to be read
+  if (CloudSerial.available()) {
+    char character = CloudSerial.read();
+    serialString += character;
+
+    // if a \n character has been received, there should be a complete command inside serialString
+    if (character == '\n') {
+      manageString();
     }
-    if(error) {
-      Serial.println("..Reconnected to the Nework!");
-     // Call the reconnect method to clean up the ArduinoCloud connection
-     ArduinoCloud.reconnect(wifiClient);
-    }
-    delay(500);
-    lastMillis = millis();
+  }
+  else // if there is nothing to read, it could be that the last command didn't end with a '\n'. Check.
+  {
+    manageString();
   }
 
-  delay(2000);
+  // Just to be able to simulate MKR1000's responses through the serial monitor
+  if (Serial.available()) {
+    CloudSerial.write(Serial.read());
+  }
+}
+
+void manageString() {
+  // Don't proceed if the string is empty
+  if (serialString.equals("")) return;
+
+  // Remove whitespaces
+  serialString.trim();
+
+  // Make it uppercase;
+  serialString.toUpperCase();
+
+  if (serialString.equals("ON")) {
+    digitalWrite(6, HIGH);
+  }
+  if (serialString.equals("OFF")) {
+    digitalWrite(6, LOW);
+  }
+
+  // Send back the command you just applied. This way the Angular frontend can stay synchronized with the MKR1000 state.
+  sendString(serialString);
+
+  // Reset serialString
+  serialString = "";
+}
+
+// sendString sends a string to the Arduino Cloud.
+void sendString(String stringToSend) {
+  // send the characters one at a time
+  char lastSentChar = 0;
+  for (int i = 0; i < stringToSend.length(); i++) {
+    lastSentChar = stringToSend.charAt(i);
+    CloudSerial.write(lastSentChar);
+  }
+
+  // if the last sent character wasn't a '\n' add it
+  if (lastSentChar != '\n') {
+    CloudSerial.write('\n');
+  }
 }
