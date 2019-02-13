75ddfccfc70ed6c514ffb4724472df6cc950f62a
Martino Facchin <m.facchin@arduino.cc>
2019-02-13 13:50:52 +0100

    Automatically handle getTime fallback in IoTCloud class

--- src/ArduinoIoTCloud.cpp
+++ src/ArduinoIoTCloud.cpp
@@ -29,7 +29,12 @@ static ConnectionManager *getTimeConnection = NULL;
 
 static unsigned long getTime() {
   if (!getTimeConnection) return 0;
-  return getTimeConnection->getTime();
+  unsigned long time = getTimeConnection->getTime();
+  if (!NTPUtils::isTimeValid(time)) {
+    debugMessage("Bogus NTP time from API, fallback to UDP method", 0);
+    time = NTPUtils(getTimeConnection->getUDP()).getTime();
+  }
+  return time;
 }
 
 ArduinoIoTCloudClass::ArduinoIoTCloudClass() :
