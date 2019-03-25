0c3369a1ee827c763e2fdd12073493dd80bd73ef
umbynos <umberto.baldi@edu.unito.it>
2019-03-25 18:13:38 +0100

    remove some comments

--- examples/MKRWIFI1010_Cloud_Blink.ino
+++ examples/MKRWIFI1010_Cloud_Blink.ino
@@ -11,10 +11,8 @@ ConnectionManager *ArduinoIoTPreferredConnection = new WiFiConnectionManager(SEC
 
 void setup(){
   setDebugMessageLevel(3);
-  //Serial.begin(9600);
   delay(1500);
   ArduinoCloud.begin(ArduinoIoTPreferredConnection);
-  //CloudSerial.begin(9600); //Already done by ArduinoIotCloud.cpp (line 332)
   CloudSerial.print("I'm ready for blinking!\n");
 }
 
@@ -63,41 +61,3 @@ void sendString(String stringToSend) {
   if (lastSentChar != '\n') {
     CloudSerial.write('\n');
   }
-}
-
-/*
-OLD                        |                     NEW
-"WiFi shield not present" --> "WiFi Hardware not available\nMake sure you are using a WiFi enabled board/shield", 0
-"Starting Arduino Cloud..." --> 
-"Attempting to connect to WPA SSID: " -->  "Connecting to \"%s\"", 2
-"Failed to connect to Wifi!" --> "Connection to \"%s\" failed", 0
-"You're connected to the network" --> "Connected to \"%s\"", 2
-"Attempting to connect to Arduino Cloud" --> "Connecting to Arduino IoT Cloud...", 0
-"Starting get time" --> "Acquiring Time from Network", 3
-"Failed to connect to Arduino Cloud!" --> "Cloud Error. Retrying...", 0
-"Successfully connected to Arduino Cloud :)" --> "Connected to Arduino IoT Cloud", 0
-
-
-
-
-*/
-
-/* se tutto va bene
-
-[ 2566 ] WiFi.status(): 0
-
-[ 2566 ] Current WiFi Firmware: 1.2.1
-
-[ 2567 ] Connecting to "BCMI"
-
-[ 12642 ] Connected to "BCMI"
-
-[ 12642 ] Acquiring Time from Network
-.
-[ 12743 ] Network Time: 1550241011
-
-[ 12938 ] Connecting to Arduino IoT Cloud...
-Compile time: 1553472000
-
-[ 15990 ] Connected to Arduino IoT Cloud
-*/
