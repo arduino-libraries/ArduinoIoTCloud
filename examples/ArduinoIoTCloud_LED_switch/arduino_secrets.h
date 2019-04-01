5b6ea6ff98d725493f1b50e00fe02731c4f10051
Alexander Entinger <cto@lxrobotics.com>
2019-04-01 11:03:43 +0200

    Enable compilation of examples for both WiFi and GSM enab.ed boards

--- examples/ArduinoIoTCloud_LED_switch/arduino_secrets.h
+++ examples/ArduinoIoTCloud_LED_switch/arduino_secrets.h
@@ -9,7 +9,7 @@
  If you prefer using a MKR GSM 1400 comment the lines above and uncommet the following.
  PIN, APN, Login and Password are supplied by your Cellular Data provider.
 */
-//#define SECRET_PIN ""
-//#define SECRET_APN ""
-//#define SECRET_LOGIN ""
-//#define SECRET_PASS ""
+#define SECRET_PIN ""
+#define SECRET_APN ""
+#define SECRET_LOGIN ""
+#define SECRET_PASS ""
--- examples/ArduinoIoTCloud_LED_switch/thingProperties.h
+++ examples/ArduinoIoTCloud_LED_switch/thingProperties.h
@@ -1,12 +1,14 @@
 #include <ArduinoIoTCloud.h>
-/*
- The following include line is used for WiFi enabled boards (MKR1000, MKR WiFi 1010)
-*/
-#include <WiFiConnectionManager.h>
-/*
- If you prefer using a MKR GSM 1400 comment the line above and uncommet the following.
-*/
-//#include <GSMConnectionManager.h>
+#include <ConnectionManager.h>
+
+#if defined(BOARD_HAS_WIFI)
+  #include <WiFiConnectionManager.h>
+#elif defined(BOARD_HAS_GSM)
+  #include <GSMConnectionManager.h>
+#else 
+  #error "Arduino IoT Cloud currently only supports MKR1000, MKR WiFi 1010 and MKR GSM 1400"
+#endif
+
 
 // Your THING_ID
 #define THING_ID "ARDUINO_IOT_CLOUD_THING_ID"
@@ -22,11 +24,10 @@ void initProperties() {
   ArduinoCloud.addProperty(potentiometer, READ, ON_CHANGE);
 }
 
-/*
- The following include line is used for WiFi enabled boards (MKR1000, MKR WiFi 1010)
-*/
-ConnectionManager *ArduinoIoTPreferredConnection = new WiFiConnectionManager(SECRET_SSID, SECRET_PASS);
-/*
- If you prefer using a MKR GSM 1400 comment the line above and uncommet the following.
-*/
-//ConnectionManager *ArduinoIoTPreferredConnection = new GSMConnectionManager(SECRET_PIN, SECRET_APN, SECRET_LOGIN, SECRET_PASS);
+#if defined(BOARD_HAS_WIFI)
+ConnectionManager * ArduinoIoTPreferredConnection = new WiFiConnectionManager(SECRET_SSID, SECRET_PASS);
+#elif defined(BOARD_HAS_GSM)
+ConnectionManager * ArduinoIoTPreferredConnection = new GSMConnectionManager (SECRET_PIN, SECRET_APN, SECRET_LOGIN, SECRET_PASS);
+#endif
+
+
