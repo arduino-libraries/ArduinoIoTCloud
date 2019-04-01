48231311dd6ac39c20432058993dd3ddccae03ac
Alexander Entinger <cto@lxrobotics.com>
2019-04-01 13:11:24 +0200

    Adding example used in conjunction with Travis CI to verify that no user facing API changes have been made

---
+++ examples/ArduinoIoTCloud_Travis_CI/arduino_secrets.h
@@ -0,0 +1,9 @@
+/* (MKR1000, MKR WiFi 1010) */
+#define SECRET_SSID "YOUR_WIFI_NETWORK_NAME"
+#define SECRET_PASS "YOUR_WIFI_PASSWORD"
+
+/* MKR GSM 1400 comment the lines above and uncommet the following. */
+#define SECRET_PIN ""
+#define SECRET_APN ""
+#define SECRET_LOGIN ""
+#define SECRET_PASS ""
