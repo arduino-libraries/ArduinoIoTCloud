6a4193e18e1f8fb502653e015701fad6fa344f7e
Alexander Entinger <cto@lxrobotics.com>
2019-04-23 06:37:46 +0200

    Simplifying getting started examples by extracting the arduino_secrets into separate file.

---
+++ examples/GSM_Cloud_Blink/arduino_secrets.h
@@ -0,0 +1,19 @@
+#ifndef SECRET_PIN
+  #define SECRET_PIN ""
+  #warning "You need to define SECRET_PIN in tab/arduino_secrets.h"
+#endif
+
+#ifndef SECRET_APN
+  #define SECRET_APN ""
+  #warning "You need to define SECRET_PIN in tab/arduino_secrets.h"
+#endif
+
+#ifndef SECRET_USER_NAME
+  #define SECRET_USER_NAME ""
+  #warning "You need to define SECRET_USER_NAME in tab/arduino_secrets.h"
+#endif
+
+#ifndef SECRET_PASSWORD
+  #define SECRET_PASSWORD ""
+  #warning "You need to define SECRET_PASSWORD in tab/arduino_secrets.h"
+#endif
---
+++ examples/WiFi_Cloud_Blink/arduino_secrets.h
@@ -0,0 +1,9 @@
+#ifndef SECRET_SSID
+  #define SECRET_SSID ""
+  #warning "You need to define SECRET_SSID in tab/arduino_secrets.h"
+#endif
+
+#ifndef SECRET_PASS
+  #define SECRET_PASS ""
+  #warning "You need to define SECRET_PASS in tab/arduino_secrets.h"
+#endif
