f55c5ec8527092a2b062b628465517c2a48abdbd
Alexander Entinger <cto@lxrobotics.com>
2020-04-30 13:37:21 +0200

    Adding method to determine the type of the provide OTAStorage medium. This is necessary for reporting to the cloud what are the available OTA options

--- extras/test/include/OTAStorage_Mock.h
+++ extras/test/include/OTAStorage_Mock.h
@@ -25,6 +25,7 @@ public:
   virtual ~OTAStorage_Mock() { }
 
 
+  virtual Type   type  () override { return Type::MKRMEM; }
   virtual bool   init  () override;
   virtual bool   open  () override;
   virtual size_t write (uint8_t const * const buf, size_t const num_bytes) override;
