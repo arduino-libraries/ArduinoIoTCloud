eaf28dc898c0ddf9a995c7c391a8d5c19983935d
Alexander Entinger <cto@lxrobotics.com>
2020-06-04 12:30:22 +0200

    Adding new class PropertyContainer which replaces built-in contained functionality of ArduinoCloudThing

---
+++ src/property/PropertyContainer.cpp
@@ -0,0 +1,151 @@
+/*
+   This file is part of ArduinoIoTCloud.
+
+   Copyright 2020 ARDUINO SA (http://www.arduino.cc/)
+
+   This software is released under the GNU General Public License version 3,
+   which covers the main part of arduino-cli.
+   The terms of this license can be found at:
+   https://www.gnu.org/licenses/gpl-3.0.en.html
+
+   You can be released from the requirements of the above licenses by purchasing
+   a commercial license. Buying such a license is mandatory if you want to modify or
+   otherwise use the software for commercial activities involving the Arduino
+   software without disclosing the source code of your own applications. To purchase
+   a commercial license, send an email to license@arduino.cc.
+*/
+
+/******************************************************************************
+   INCLUDE
+ ******************************************************************************/
+
+#include "PropertyContainer.h"
+
+#include <algorithm>
+
+#include "../cbor/types/CloudWrapperBase.h"
+
+/******************************************************************************
+   CTOR/DTOR
+ ******************************************************************************/
+
+PropertyContainer::PropertyContainer()
+: _numProperties{0}
+, _numPrimitivesProperties{0}
+, _get_time_func{nullptr}
+{
+
+}
+
+/******************************************************************************
+   PUBLIC MEMBER FUNCTIONS
+ ******************************************************************************/
+
+void PropertyContainer::begin(GetTimeCallbackFunc func)
+{
+  _get_time_func = func;
+}
+
+ArduinoCloudProperty & PropertyContainer::addPropertyReal(ArduinoCloudProperty & property, String const & name, Permission const permission, int propertyIdentifier)
+{
+  /* Check wether or not the property already has been added to the container */
+  ArduinoCloudProperty * p = getProperty(name);
+  if(p != nullptr) return (*p);
+
+  /* Initialize property and add it to the container */
+  property.init(name, permission, _get_time_func);
+
+  if (property.isPrimitive()) { _numPrimitivesProperties++; }
+  _numProperties++;
+  addProperty(&property, propertyIdentifier);
+  return property;
+}
+
+
+ArduinoCloudProperty * PropertyContainer::getProperty(String const & name)
+{
+  std::list<ArduinoCloudProperty *>::iterator iter;
+
+  iter = std::find_if(_property_list.begin(),
+                      _property_list.end(),
+                      [name](ArduinoCloudProperty * p) -> bool
+                      {
+                        return (p->name() == name);
+                      });
+
+  if (iter == _property_list.end())
+    return nullptr;
+  else
+    return (*iter);
+}
+
+ArduinoCloudProperty * PropertyContainer::getProperty(int const identifier)
+{
+  std::list<ArduinoCloudProperty *>::iterator iter;
+
+  iter = std::find_if(_property_list.begin(),
+                      _property_list.end(),
+                      [identifier](ArduinoCloudProperty * p) -> bool
+                      {
+                        return (p->identifier() == identifier);
+                      });
+
+  if (iter == _property_list.end())
+    return nullptr;
+  else
+    return (*iter);
+}
+
+int PropertyContainer::appendChangedProperties(CborEncoder * arrayEncoder, bool lightPayload)
+{
+  int appendedProperties = 0;
+  std::for_each(_property_list.begin(),
+                _property_list.end(),
+                [arrayEncoder, lightPayload, &appendedProperties](ArduinoCloudProperty * p)
+                {
+                  if (p->shouldBeUpdated() && p->isReadableByCloud())
+                  {
+                    p->append(arrayEncoder, lightPayload);
+                    appendedProperties++;
+                  }
+                });
+  return appendedProperties;
+}
+
+void PropertyContainer::updateTimestampOnLocallyChangedProperties()
+{
+  /* This function updates the timestamps on the primitive properties 
+   * that have been modified locally since last cloud synchronization
+   */
+  if (_numPrimitivesProperties > 0)
+  {
+    std::for_each(_property_list.begin(),
+                  _property_list.end(),
+                  [](ArduinoCloudProperty * p)
+                  {
+                    CloudWrapperBase * pbase = reinterpret_cast<CloudWrapperBase *>(p);
+                    if (pbase->isPrimitive() && pbase->isChangedLocally() && pbase->isReadableByCloud())
+                    {
+                      p->updateLocalTimestamp();
+                    }
+                  });
+  }
+}
+
+/******************************************************************************
+   PRIVATE MEMBER FUNCTIONS
+ ******************************************************************************/
+
+void PropertyContainer::addProperty(ArduinoCloudProperty * property_obj, int propertyIdentifier)
+{
+  if (propertyIdentifier != -1)
+  {
+    property_obj->setIdentifier(propertyIdentifier);
+  }
+  /* If property identifier is -1, an incremental value will be assigned as identifier. */
+  else
+  {
+    property_obj->setIdentifier(_numProperties);
+  }
+  _property_list.push_back(property_obj);
+}
---
+++ src/property/PropertyContainer.h
@@ -0,0 +1,69 @@
+/*
+   This file is part of ArduinoIoTCloud.
+
+   Copyright 2020 ARDUINO SA (http://www.arduino.cc/)
+
+   This software is released under the GNU General Public License version 3,
+   which covers the main part of arduino-cli.
+   The terms of this license can be found at:
+   https://www.gnu.org/licenses/gpl-3.0.en.html
+
+   You can be released from the requirements of the above licenses by purchasing
+   a commercial license. Buying such a license is mandatory if you want to modify or
+   otherwise use the software for commercial activities involving the Arduino
+   software without disclosing the source code of your own applications. To purchase
+   a commercial license, send an email to license@arduino.cc.
+*/
+
+#ifndef ARDUINO_PROPERTY_CONTAINER_H_
+#define ARDUINO_PROPERTY_CONTAINER_H_
+
+/******************************************************************************
+   INCLUDE
+ ******************************************************************************/
+
+#include "../cbor/ArduinoCloudProperty.h"
+
+#undef max
+#undef min
+#include <list>
+
+/******************************************************************************
+   CLASS DECLARATION
+ ******************************************************************************/
+
+class PropertyContainer
+{
+
+public:
+
+  PropertyContainer();
+
+  
+  void begin(GetTimeCallbackFunc func);
+
+
+  ArduinoCloudProperty & addPropertyReal(ArduinoCloudProperty & property, String const & name, Permission const permission, int propertyIdentifier = -1);
+
+  
+  ArduinoCloudProperty * getProperty          (String const & name);
+  ArduinoCloudProperty * getProperty          (int const identifier);
+
+
+  int appendChangedProperties(CborEncoder * arrayEncoder, bool lightPayload);
+  void updateTimestampOnLocallyChangedProperties();
+
+
+
+private:
+
+  int _numProperties;
+  int _numPrimitivesProperties;
+  GetTimeCallbackFunc _get_time_func;
+  std::list<ArduinoCloudProperty *> _property_list;
+
+  void addProperty(ArduinoCloudProperty * property_obj, int propertyIdentifier);
+
+};
+
+#endif /* ARDUINO_PROPERTY_CONTAINER_H_ */
