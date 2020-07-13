d1abdfef34450cd075c4b0c19f2fc9fb18d462e2
Alexander Entinger <cto@lxrobotics.com>
2020-07-13 13:47:47 +0200

    Converting lzss.c from to linux line endings via dos2unix and replacing main with functions for encoding/decoding to be called from a python script

--- extras/tools/lzss.c
+++ extras/tools/lzss.c
@@ -153,28 +153,34 @@ void decode(void)
     }
 }
 
-int main(int argc, char *argv[])
+int encode_file(char const * in, char const * out)
 {
-    int enc;
-    char *s;
-    
-    if (argc != 4) {
-        printf("Usage: lzss e/d infile outfile\n\te = encode\td = decode\n");
-        return 1;
-    }
-    s = argv[1];
-    if (s[1] == 0 && (*s == 'd' || *s == 'D' || *s == 'e' || *s == 'E'))
-        enc = (*s == 'e' || *s == 'E');
-    else {
-        printf("? %s\n", s);  return 1;
-    }
-    if ((infile  = fopen(argv[2], "rb")) == NULL) {
-        printf("? %s\n", argv[2]);  return 1;
-    }
-    if ((outfile = fopen(argv[3], "wb")) == NULL) {
-        printf("? %s\n", argv[3]);  return 1;
-    }
-    if (enc) encode();  else decode();
-    fclose(infile);  fclose(outfile);
+    infile = fopen(in, "rb");
+    if (infile == NULL) return 0;
+
+    outfile = fopen(out, "wb");
+    if (outfile == NULL) return 0;
+
+    encode();
+
+    fclose(infile);
+    fclose(outfile);
+
+    return 0;
+}
+
+int decode_file(char const * in, char const * out)
+{
+    infile = fopen(in, "rb");
+    if (infile == NULL) return 0;
+
+    outfile = fopen(out, "wb");
+    if (outfile == NULL) return 0;
+
+    decode();
+
+    fclose(infile);
+    fclose(outfile);
+
     return 0;
 }
