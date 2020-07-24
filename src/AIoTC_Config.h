4729b858067d1d608d5f51b5c6becc6f95d5be15
Alexander Entinger <cto@lxrobotics.com>
2020-07-24 10:38:09 +0200

    Add PortentaH7 target (#179)
    Co-authored-by: Martino Facchin <m.facchin@arduino.cc>

--- src/AIoTC_Config.h
+++ src/AIoTC_Config.h
@@ -70,7 +70,8 @@
 
 #if defined(ARDUINO_SAMD_MKRGSM1400) || defined(ARDUINO_SAMD_MKRWIFI1010) ||   \
   defined(ARDUINO_SAMD_MKR1000) || defined(ARDUINO_SAMD_NANO_33_IOT)      ||   \
-  defined(ARDUINO_SAMD_MKRNB1500)
+  defined(ARDUINO_SAMD_MKRNB1500) || defined(ARDUINO_PORTENTA_H7_M7)      ||   \
+  defined(ARDUINO_PORTENTA_H7_M4)
   #define BOARD_HAS_ECCX08
   #define HAS_TCP
 #endif
