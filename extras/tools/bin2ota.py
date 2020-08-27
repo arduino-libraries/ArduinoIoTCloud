735899c98507bca3779da0a58e9f6600933fdc8b
Alexander Entinger <cto@lxrobotics.com>
2020-08-27 14:58:55 +0200

    Tailor bin2ota script for MKR WiFi 1010 and new OTA header structure

--- extras/tools/bin2ota.py
+++ extras/tools/bin2ota.py
@@ -15,13 +15,24 @@ in_file = open(ifile, "rb")
 bin_data = bytearray(in_file.read())
 in_file.close()
 
+# Magic number (VID/PID), hard coded for MKR VIDOR 4000 right now
+magic_number = bytearray([0x23, 0x41, 0x80, 0x54])
+print(magic_number)
+
+# Version field (byte array of size 8)
+version = bytearray(8)
+print(version)
+
+# Prepend magic number and version field to payload
+bin_data_complete = magic_number + version + bin_data
+
 # Calculate length and CRC32
-bin_data_len = len(bin_data)
-bin_data_crc = crccheck.crc.Crc32.calc(bin_data)
+bin_data_len = len(bin_data_complete)
+bin_data_crc = crccheck.crc.Crc32.calc(bin_data_complete)
 
 # Write to outfile
 out_file = open(ofile, "wb")
 out_file.write((bin_data_len).to_bytes(4,byteorder='little'))
 out_file.write((bin_data_crc).to_bytes(4,byteorder='little'))
-out_file.write(bin_data)
+out_file.write(bin_data_complete)
 out_file.close()
