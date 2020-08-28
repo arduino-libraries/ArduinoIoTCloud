d08c13497477db2068a79ca48b9ce3c285b39bc3
Alexander Entinger <cto@lxrobotics.com>
2020-08-28 07:45:46 +0200

    Adjusting bin2ota.py to support both MKR WIFI 1010 and Nano 33 IOT

--- extras/tools/bin2ota.py
+++ extras/tools/bin2ota.py
@@ -3,12 +3,14 @@
 import sys
 import crccheck
 
-if len(sys.argv) != 3:
-    print ("Usage: bin2ota.py sketch.bin sketch.ota")
+if len(sys.argv) != 4:
+    print ("Usage: bin2ota.py BOARD sketch.bin sketch.ota")
+    print ("  BOARD = [MKR_WIFI_1010 | NANO_33_IOT]")
     sys.exit()
 
-ifile = sys.argv[1]
-ofile = sys.argv[2]
+board = sys.argv[1]
+ifile = sys.argv[2]
+ofile = sys.argv[3]
 
 # Read the binary file
 in_file = open(ifile, "rb")
@@ -16,12 +18,19 @@ bin_data = bytearray(in_file.read())
 in_file.close()
 
 # Magic number (VID/PID), hard coded for MKR VIDOR 4000 right now
-magic_number = bytearray([0x23, 0x41, 0x80, 0x54])
-print(magic_number)
+magic_number_mkr_wifi_1010 = bytearray([0x23, 0x41, 0x80, 0x54])
+magic_number_nano_33_iot   = bytearray([0x23, 0x41, 0x80, 0x57])
+
+if board == "MKR_WIFI_1010":
+    magic_number = magic_number_mkr_wifi_1010
+elif board == "NANO_33_IOT":
+    magic_number = magic_number_nano_33_iot
+else:
+    print ("Error,", board, "is not a supported board type")
+    sys.exit()
 
 # Version field (byte array of size 8)
 version = bytearray(8)
-print(version)
 
 # Prepend magic number and version field to payload
 bin_data_complete = magic_number + version + bin_data
