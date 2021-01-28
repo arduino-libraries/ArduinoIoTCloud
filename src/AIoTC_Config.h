7076c27f75b49e74a699b56b5ed9cc14c89d9f1a
Alexander Entinger <cto@lxrobotics.com>
2021-01-28 12:28:05 +0100

    Ensure that OTA_ENABLE is set correctly.

--- src/AIoTC_Config.h
+++ src/AIoTC_Config.h
@@ -102,12 +102,11 @@
 
 #if defined(ARDUINO_PORTENTA_H7_M7)
   #define OTA_STORAGE_PORTENTA_QSPI   (1)
-  #define OTA_STORAGE_PORTENTA        (1)
 #else
   #define OTA_STORAGE_PORTENTA_QSPI   (0)
 #endif
 
-#if (OTA_STORAGE_SFU || OTA_STORAGE_SSU || OTA_STORAGE_SNU) && !defined(ARDUINO_AVR_UNO_WIFI_REV2)
+#if (OTA_STORAGE_SFU || OTA_STORAGE_SSU || OTA_STORAGE_SNU || OTA_STORAGE_PORTENTA_QSPI) && !defined(ARDUINO_AVR_UNO_WIFI_REV2)
   #define OTA_ENABLED             (1)
 #else
   #define OTA_ENABLED             (0)
