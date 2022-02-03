8439b010658dd86efe7b4280bade2a1f3816e5b7
pennam <m.pennasilico@arduino.cc>
2022-02-03 16:02:29 +0100

    setTimeZoneData cosmetic changes

--- src/utility/time/TimeService.cpp
+++ src/utility/time/TimeService.cpp
@@ -156,15 +156,12 @@ void TimeService::setSyncInterval(unsigned long seconds)
 
 void TimeService::setTimeZoneData(long offset, unsigned long dst_until)
 {
-  if(_timezone_offset != offset)
-    DEBUG_DEBUG("ArduinoIoTCloudTCP::%s tz_offset: [%d]", __FUNCTION__, offset);
-  _timezone_offset = offset;
-
-  if(_timezone_dst_until != dst_until)
-    DEBUG_DEBUG("ArduinoIoTCloudTCP::%s tz_dst_unitl: [%ul]", __FUNCTION__, dst_until);
-  _timezone_dst_until = dst_until;
-
-  _is_tz_configured = true;
+  if(_timezone_offset != offset || _timezone_dst_until != dst_until) {
+    DEBUG_DEBUG("ArduinoIoTCloudTCP::%s offset: %d dst_unitl %ul", __FUNCTION__, offset, dst_until);
+    _timezone_offset = offset;
+    _timezone_dst_until = dst_until;
+    _is_tz_configured = true;
+  }
 }
 
 unsigned long TimeService::getLocalTime()
