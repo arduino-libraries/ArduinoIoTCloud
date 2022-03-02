7f72d757c8051a09ec0ae1a1aeaa9f6ccfecb61a
pennam <m.pennasilico@arduino.cc>
2022-03-02 14:53:32 +0100

    CryptoUtil: include in the build and add support for devices using SE050 crypto

--- src/tls/utility/CryptoUtil.h
+++ src/tls/utility/CryptoUtil.h
@@ -24,20 +24,34 @@
 
 #include <AIoTC_Config.h>
 
-#if defined(BOARD_HAS_ECCX08) || defined(BOARD_HAS_OFFLOADED_ECCX08)
+#if defined(BOARD_HAS_ECCX08) || defined(BOARD_HAS_OFFLOADED_ECCX08) || defined(BOARD_HAS_SE050)
 #include <Arduino.h>
 #include "Cert.h"
+
+#if defined(BOARD_HAS_SE050)
+#include <SE05X.h>
+#else
 #include <ArduinoECCX08.h>
+#endif
+
+/******************************************************************************
+ * DEFINE
+ ******************************************************************************/
+#if defined(BOARD_HAS_SE050)
+#define CRYPTO_SLOT_OFFSET                100
+#else
+#define CRYPTO_SLOT_OFFSET                0
+#endif
 
 /******************************************************************************
    TYPEDEF
  ******************************************************************************/
 enum class CryptoSlot : int
 {
-  Key                                   = 0,
-  CompressedCertificate                 = 10,
-  SerialNumberAndAuthorityKeyIdentifier = 11,
-  DeviceId                              = 12
+  Key                                   = (0  + CRYPTO_SLOT_OFFSET),
+  CompressedCertificate                 = (10 + CRYPTO_SLOT_OFFSET),
+  SerialNumberAndAuthorityKeyIdentifier = (11 + CRYPTO_SLOT_OFFSET),
+  DeviceId                              = (12 + CRYPTO_SLOT_OFFSET)
 };
 
 /******************************************************************************
@@ -64,10 +78,14 @@ public:
   int readCert(ArduinoIoTCloudCertClass & cert, const CryptoSlot certSlot);
 
 private:
+#if defined(BOARD_HAS_SE050)
+  SE05XClass & _crypto;
+#else
   ECCX08Class & _crypto;
+#endif
 
 };
 
-#endif /* BOARD_HAS_ECCX08 || BOARD_HAS_OFFLOADED_ECCX08 */
+#endif /* BOARD_HAS_ECCX08 || BOARD_HAS_OFFLOADED_ECCX08 || BOARD_HAS_SE050 */
 
 #endif /* ARDUINO_IOT_CLOUD_UTILITY_CRYPTO_CRYPTO_UTIL_H_ */
