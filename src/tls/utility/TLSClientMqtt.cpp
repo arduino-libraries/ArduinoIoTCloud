30602e98ea724b222b938a4aa757001bb041bb8d
pennam <m.pennasilico@arduino.cc>
2023-05-15 09:55:32 +0200

    Initial: add pi pico w support

--- src/tls/utility/TLSClientMqtt.cpp
+++ src/tls/utility/TLSClientMqtt.cpp
@@ -59,6 +59,8 @@ void TLSClientMqtt::begin(ConnectionHandler & connection) {
    * https://github.com/arduino/uno-r4-wifi-usb-bridge/blob/f09ca94fdcab845b8368d4435fdac9f6999d21d2/certificates/certificates.pem#L852
    */
   (void)connection;
+#elif defined(ARDUINO_RASPBERRY_PI_PICO_W)
+    setCACert(AIoTUPCert);
 #elif defined(ARDUINO_ARCH_ESP32)
   #if (ESP_ARDUINO_VERSION < ESP_ARDUINO_VERSION_VAL(3, 0, 4))
     setCACertBundle(x509_crt_bundle);
--- src/utility/time/TimeService.cpp
+++ src/utility/time/TimeService.cpp
@@ -341,6 +341,7 @@ void TimeServiceClass::initRTC()
   esp8266_initRTC();
 #elif defined (ARDUINO_ARCH_RENESAS)
   renesas_initRTC();
+#elif defined (ARDUINO_RASPBERRY_PI_PICO_W)
 #else
   #error "RTC not available for this architecture"
 #endif
@@ -358,6 +359,7 @@ void TimeServiceClass::setRTC(unsigned long time)
   esp8266_setRTC(time);
 #elif defined (ARDUINO_ARCH_RENESAS)
   renesas_setRTC(time);
+#elif defined (ARDUINO_RASPBERRY_PI_PICO_W)
 #else
   #error "RTC not available for this architecture"
 #endif
@@ -375,6 +377,8 @@ unsigned long TimeServiceClass::getRTC()
   return esp8266_getRTC();
 #elif defined (ARDUINO_ARCH_RENESAS)
   return renesas_getRTC();
+#elif defined (ARDUINO_RASPBERRY_PI_PICO_W)
+  return 1;
 #else
   #error "RTC not available for this architecture"
 #endif
