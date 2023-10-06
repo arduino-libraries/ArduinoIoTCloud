0c21a29c7e3b4df2d8bff9db76eacdec798ccda2
pennam <m.pennasilico@arduino.cc>
2023-10-06 14:01:05 +0200

    Reorder timeout variables declaration and initialization

--- src/ArduinoIoTCloudTCP.h
+++ src/ArduinoIoTCloudTCP.h
@@ -132,10 +132,10 @@ class ArduinoIoTCloudTCP: public ArduinoIoTCloudClass
     unsigned int _last_connection_attempt_cnt;
     unsigned long _next_device_subscribe_attempt_tick;
     unsigned int _last_device_subscribe_cnt;
-    unsigned long _last_sync_request_tick;
-    unsigned int _last_sync_request_cnt;
     unsigned long _next_thing_subscribe_attempt_tick;
     unsigned int  _last_thing_subscribe_attempt_cnt;
+    unsigned long _last_sync_request_tick;
+    unsigned int _last_sync_request_cnt;
     String _brokerAddress;
     uint16_t _brokerPort;
     uint8_t _mqtt_data_buf[MQTT_TRANSMIT_BUFFER_SIZE];
