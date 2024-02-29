94df318eee139d8ec208be61fa360bdffccd2bd5
Andrea Gilardoni <a.gilardoni@arduino.cc>
2024-02-29 13:48:08 +0100

    implemented unor4 ota class

--- src/ota/implementation/OTAUnoR4.h
+++ src/ota/implementation/OTAUnoR4.h
@@ -1,22 +1,52 @@
+/*
+  This file is part of the ArduinoIoTCloud library.
+
+  Copyright (c) 2024 Arduino SA
+
+  This Source Code Form is subject to the terms of the Mozilla Public
+  License, v. 2.0. If a copy of the MPL was not distributed with this
+  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+*/
+
 #pragma once
 
-#include "src/ota/interface/OTAInterface.h"
+#include "ota/interface/OTAInterface.h"
 #include "OTAUpdate.h"
 #include "r_flash_lp.h"
 
 class UNOR4OTACloudProcess: public OTACloudProcessInterface {
 public:
-  STM32H7OTACloudProcess();
-protected:
-  // we start the download and decompress process
-  virtual State fetch(Message* msg=nullptr);
+  UNOR4OTACloudProcess(MessageStream *ms);
 
-  // when the download is completed we verify for integrity and correctness of the downloaded binary
-  // virtual State verifyOTA(Message* msg=nullptr); // TODO this may be performed inside download
+  bool isOtaCapable() override;
+protected:
+  virtual OTACloudProcessInterface::State resume(Message* msg=nullptr) override;
+
+  // we are overriding the method of startOTA in order to download ota file on ESP32
+  virtual OTACloudProcessInterface::State startOTA() override;
+
+  // we start the download and decompress process
+  virtual OTACloudProcessInterface::State fetch() override;
 
   // whene the download is correctly finished we set the mcu to use the newly downloaded binary
-  virtual State flashOTA(Message* msg=nullptr);
+  virtual OTACloudProcessInterface::State flashOTA() override;
 
   // we reboot the device
-  virtual State reboot(Message* msg=nullptr);
+  virtual OTACloudProcessInterface::State reboot() override;
+
+  virtual void reset() override;
+
+  constexpr void* appStartAddress();
+  uint32_t appSize();
+
+  bool appFlashOpen();
+  bool appFlashClose();
+
+public:
+  // used to access to flash memory for sha256 calculation
+  flash_lp_instance_ctrl_t  ctrl;
+  flash_cfg_t               cfg;
+
+  OTAUpdate ota;
+  static const char UPDATE_FILE_NAME[];
 };
