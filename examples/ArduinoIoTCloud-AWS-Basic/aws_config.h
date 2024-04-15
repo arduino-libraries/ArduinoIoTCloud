97f42eb4bb83168199f0d68e4c82ae5daf73550c
pennam <m.pennasilico@arduino.cc>
2024-04-15 21:49:46 +0200

    Add example to connecto to ArduinoIoTCloud and AWS

---
+++ examples/ArduinoIoTCloud-AWS-Basic/aws_config.h
@@ -0,0 +1,10 @@
+/* Fill in the hostname of your AWS IoT broker */
+#define AWS_BROKER ""
+
+#define AWS_SLOT 4
+
+/* Fill in the boards public certificate */
+const char AWS_CERTIFICATE[] = R"(
+-----BEGIN CERTIFICATE-----
+-----END CERTIFICATE-----
+)";
