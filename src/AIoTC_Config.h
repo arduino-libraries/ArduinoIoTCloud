501271b5a868c178595265d4c83b0777aba8fe28
pennam <m.pennasilico@arduino.cc>
2025-03-03 08:59:28 +0100

    Release v2.4.1

--- src/AIoTC_Config.h
+++ src/AIoTC_Config.h
@@ -183,6 +183,6 @@
   #define AIOT_CONFIG_LASTVALUES_SYNC_MAX_RETRY_CNT                  (10UL)
 #endif
 
-#define AIOT_CONFIG_LIB_VERSION "2.4.0"
+#define AIOT_CONFIG_LIB_VERSION "2.4.1"
 
 #endif /* ARDUINO_AIOTC_CONFIG_H_ */
