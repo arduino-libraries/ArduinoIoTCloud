ebd72c11b7c50a3297e0005224c9d87e5805971b
pennam <m.pennasilico@arduino.cc>
2025-04-16 14:45:20 +0200

    Provisioning v0.1.1

--- examples/utility/Provisioning_2.0/Provisioning_2.0.ino
+++ examples/utility/Provisioning_2.0/Provisioning_2.0.ino
@@ -15,7 +15,7 @@
 #include <utility/SElementArduinoCloudCertificate.h>
 #include "utility/LEDFeedback.h"
 
-const char *SKETCH_VERSION = "0.1.0";
+const char *SKETCH_VERSION = "0.1.1";
 
 enum class DeviceState {
     HARDWARE_CHECK,
