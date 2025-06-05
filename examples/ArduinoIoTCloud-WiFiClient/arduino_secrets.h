4d6be98e02bdc0ad20e43ad84e40313b9b4a4da3
pennam <m.pennasilico@arduino.cc>
2025-06-05 17:03:14 +0200

    add example using WiFi Client

---
+++ examples/ArduinoIoTCloud-WiFiClient/arduino_secrets.h
@@ -0,0 +1,9 @@
+/* A complete list of supported boards with WiFi is available here:
+ * https://github.com/arduino-libraries/ArduinoIoTCloud/#what
+ */
+
+#define SECRET_WIFI_SSID "YOUR_WIFI_NETWORK_NAME"
+#define SECRET_WIFI_PASS "YOUR_WIFI_PASSWORD"
+
+/* Configure this if you want to use username/password broker authentication */
+#define SECRET_DEVICE_KEY "my-device-password"
