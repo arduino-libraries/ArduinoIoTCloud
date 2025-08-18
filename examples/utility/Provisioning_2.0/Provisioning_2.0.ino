b6669350fbcc2ed4038f4cc76fa7dfc154e4cb41
fabik111 <fabiomassimo.centonze@gmail.com>
2025-08-18 10:41:41 +0200

    Provisioning v0.3.3

--- examples/utility/Provisioning_2.0/Provisioning_2.0.ino
+++ examples/utility/Provisioning_2.0/Provisioning_2.0.ino
@@ -15,7 +15,7 @@
 #include <utility/SElementArduinoCloudCertificate.h>
 #include "utility/LEDFeedback.h"
 
-const char *SKETCH_VERSION = "0.3.0";
+const char *SKETCH_VERSION = "0.3.3";
 
 enum class DeviceState {
     HARDWARE_CHECK,
