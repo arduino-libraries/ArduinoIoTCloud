24fb0bd5095a92b7d72785abe14e37daca388fa6
pennam <m.pennasilico@arduino.cc>
2025-09-16 10:32:37 +0200

    library: change license to MPL
    Add missing file

--- extras/test/include/Arduino_ConnectionHandler.h
+++ extras/test/include/Arduino_ConnectionHandler.h
@@ -1,12 +1,18 @@
 /*
-   Copyright (c) 2019 Arduino.  All rights reserved.
+  This file is part of the ArduinoIoTCloud library.
+
+  Copyright (c) 2019 Arduino SA
+
+  This Source Code Form is subject to the terms of the Mozilla Public
+  License, v. 2.0. If a copy of the MPL was not distributed with this
+  file, You can obtain one at http://mozilla.org/MPL/2.0/.
 */
 
 #ifndef TEST_ARDUINO_CONNECTION_HANDLER_H_
 #define TEST_ARDUINO_CONNECTION_HANDLER_H_
 
 /******************************************************************************
-   TYPEDEF
+  TYPEDEF
  ******************************************************************************/
 
 typedef void ConnectionHandler;
