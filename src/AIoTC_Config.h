7882ddd57b7be1cef1b5b64e1b88ca89842ec6ce
fabik111 <fabiomassimo.centonze@gmail.com>
2026-02-24 17:05:06 +0100

    Release v2.9.0

--- src/AIoTC_Config.h
+++ src/AIoTC_Config.h
@@ -168,6 +168,6 @@
   #define AIOT_CONFIG_LASTVALUES_SYNC_MAX_RETRY_CNT                  (10UL)
 #endif
 
-#define AIOT_CONFIG_LIB_VERSION "2.8.0"
+#define AIOT_CONFIG_LIB_VERSION "2.9.0"
 
 #endif /* ARDUINO_AIOTC_CONFIG_H_ */
